a8e030fc88595a8d01f8ec3b77b1d30074616ca5
Harun Bekcan <harunbekcan@gmail.com>
2023-06-01 00:49:42 +0300

    navDrawer completed.

--- app/src/main/java/com/harunbekcan/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/navigationdrawer/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.harunbekcan.navigationdrawer
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.harunbekcan.navigationdrawer.base.BaseActivity
 import com.harunbekcan.navigationdrawer.databinding.ActivityMainBinding
+import com.harunbekcan.navigationdrawer.databinding.NavigationHeaderBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     private lateinit var actionDrawerToggle : ActionBarDrawerToggle
+    private lateinit var navigationHeaderBinding: NavigationHeaderBinding
 
     override fun prepareView(savedInstanceState: Bundle?) {
+        navigationHeaderBinding = NavigationHeaderBinding.bind(binding.navigationView.getHeaderView(0))
         setDrawerToggle()
+        navHeaderLayoutMenuItemClicked()
     }
 
     private fun setDrawerToggle() {
@@ -20,6 +25,18 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             drawerLayout.addDrawerListener(actionDrawerToggle)
             actionDrawerToggle.syncState()
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun navHeaderLayoutMenuItemClicked(){
+       navigationHeaderBinding.navHeaderMenu.apply {
+            cameraContainer.setOnClickListener {
+                    Log.d("HARUNHARUN","Clicked On Camera")
+                }
+
+            galleryContainer.setOnClickListener {
+                Log.d("HARUNHARUN","Clicked On Gallery")
+            }
         }
     }
 
