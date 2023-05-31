4b4cc8f2c4f029d7438b49a8a78d10c9800d6574
Harun Bekcan <harunbekcan@gmail.com>
2023-05-31 21:46:06 +0300

    navDrawerUi completed.

--- app/src/main/java/com/harunbekcan/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/navigationdrawer/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.harunbekcan.navigationdrawer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.harunbekcan.navigationdrawer.base.BaseActivity
+import com.harunbekcan.navigationdrawer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    private lateinit var actionDrawerToggle : ActionBarDrawerToggle
+
+    override fun prepareView(savedInstanceState: Bundle?) {
+        setDrawerToggle()
     }
+
+    private fun setDrawerToggle() {
+        binding.apply {
+            actionDrawerToggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(actionDrawerToggle)
+            actionDrawerToggle.syncState()
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
