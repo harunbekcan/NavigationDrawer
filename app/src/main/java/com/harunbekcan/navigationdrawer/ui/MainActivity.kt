5477e960deb33194850850c65c5f077497dbefea
Harun Bekcan <harunbekcan@gmail.com>
2023-06-01 11:24:13 +0300

    sampleNavigationDrawer completed.

--- app/src/main/java/com/harunbekcan/navigationdrawer/ui/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/navigationdrawer/ui/MainActivity.kt
@@ -1,28 +1,30 @@
 package com.harunbekcan.navigationdrawer.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.harunbekcan.navigationdrawer.R
 import com.harunbekcan.navigationdrawer.base.BaseActivity
 import com.harunbekcan.navigationdrawer.databinding.ActivityMainBinding
 import com.harunbekcan.navigationdrawer.databinding.NavigationHeaderBinding
+import com.harunbekcan.navigationdrawer.utils.showToastMessage
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
-    private lateinit var actionDrawerToggle : ActionBarDrawerToggle
+    private lateinit var actionDrawerToggle: ActionBarDrawerToggle
     private lateinit var navigationHeaderBinding: NavigationHeaderBinding
 
     override fun prepareView(savedInstanceState: Bundle?) {
-        navigationHeaderBinding = NavigationHeaderBinding.bind(binding.navigationView.getHeaderView(0))
+        navigationHeaderBinding =
+            NavigationHeaderBinding.bind(binding.navigationView.getHeaderView(0))
         setDrawerToggle()
         navHeaderLayoutMenuItemClicked()
     }
 
     private fun setDrawerToggle() {
         binding.apply {
-            actionDrawerToggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout,
+            actionDrawerToggle = ActionBarDrawerToggle(
+                this@MainActivity, drawerLayout,
                 R.string.open,
                 R.string.close
             )
@@ -32,20 +34,28 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         }
     }
 
-    private fun navHeaderLayoutMenuItemClicked(){
-       navigationHeaderBinding.navHeaderMenu.apply {
+    private fun navHeaderLayoutMenuItemClicked() {
+        navigationHeaderBinding.navHeaderMenu.apply {
             cameraContainer.setOnClickListener {
-                    Log.d("HARUNHARUN","Clicked On Camera")
-                }
+                showToastMessage(message = "Clicked Camera")
+            }
 
             galleryContainer.setOnClickListener {
-                Log.d("HARUNHARUN","Clicked On Gallery")
+                showToastMessage(message = "Clicked Gallery")
+            }
+
+            shareContainer.setOnClickListener {
+                showToastMessage(message = "Clicked Share")
+            }
+
+            slideShowContainer.setOnClickListener {
+                showToastMessage(message = "Clicked SlideShow")
             }
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (actionDrawerToggle.onOptionsItemSelected(item)){
+        if (actionDrawerToggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
